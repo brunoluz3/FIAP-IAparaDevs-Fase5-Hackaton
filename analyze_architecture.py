8cc8d9871c7db687d7b8c77aeb9d81fa037febde
brunoluz3 <bruno.luz3@gmail.com>
2025-09-25 01:23:54 -0300

    geracao do report

--- analyze_architecture.py
+++ analyze_architecture.py
@@ -1,6 +1,7 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 from rag import semantic_search_STRIDE
+import aspose.pdf as ap
 import base64
 import os
 
@@ -50,14 +51,15 @@ def analyze_image(image_path):
 
 def solve_vulnerabilities(image_path):
     
-    # Você é um arquiteto de software senior e renomado no mercado de tecnologia. 
-    #     Você precisa gerar diagrama em texto (Mermaid) e
-    
-    command = """Monte um prompt objetivo e curto para que o dall-e gere um 
-        diagrama utilizando a mesma cloud da imagem origem aplicando as correções das vulnerabilidades           
+    command = """
+            Você é um especialista em arquitetura de software, com base no levantamento da solução abaixo, você deve 
+            montar um diagrama marmeid com a descrição dos componente, como eles se relacionam e as correções dos pontos
+            de vulnerabilidades           
      """   
 
-    prompt = command + analyze_image(image_path)
+    current_solution = analyze_image(image_path)
+
+    prompt = command + current_solution
     
     response = client.chat.completions.create(
         model='gpt-5',
@@ -68,21 +70,63 @@ def solve_vulnerabilities(image_path):
             ]
         }],        
     )
+    
+    improvement = response.choices[0].message.content
 
-    print(response.choices[0].message.content)
-    # return response.choices[0].message.content
+    print(improvement)
+    generate_report(image_path, current_solution, improvement)
 
-       
-    # response = client.images.generate(
-    # model="dall-e-3",
-    # prompt=response.choices[0].message.content,
-    # size="1024x1024",
-    # n=1,
-    # )
+def generate_report (image_path, current_solution, improvement):
+    complete_file_name = os.path.basename(image_path)
+    file_name, _ = os.path.splitext(complete_file_name)
 
-    # print(response.data[0].url)
-    # return response.data[0].url
+    document = ap.Document()   
+    page = document.pages.add()    
+    
+    report = ap.text.TextFragment("Relatório de análise da solução: " + file_name )
+    report.text_state.font_style = ap.text.FontStyles.BOLD
+    report.position = ap.text.Position(200, 800)
+    page.paragraphs.add(report)
+    page.paragraphs.add(ap.text.TextFragment(""))  
+    page.paragraphs.add(ap.text.TextFragment("")) 
+    page.paragraphs.add(ap.text.TextFragment("")) 
 
+    current_solution_title = ap.text.TextFragment("Análise completa da solução atual")
+    current_solution_title.text_state.font_style = ap.text.FontStyles.BOLD
+    current_solution_title.position = ap.text.Position(90, 450)
+    page.paragraphs.add(current_solution_title)
+    page.paragraphs.add(ap.text.TextFragment("")) 
+    page.paragraphs.add(ap.text.TextFragment(current_solution)) 
+    page.paragraphs.add(ap.text.TextFragment(""))  
+    page.paragraphs.add(ap.text.TextFragment("")) 
+
+    improvement_title = ap.text.TextFragment("Sugestões de melhorias")
+    improvement_title.text_state.font_style = ap.text.FontStyles.BOLD
+    page.paragraphs.add(improvement_title)
+    page.paragraphs.add(ap.text.TextFragment("")) 
+    page.paragraphs.add(ap.text.TextFragment(improvement)) 
+    page.paragraphs.add(ap.text.TextFragment(""))  
+    page.paragraphs.add(ap.text.TextFragment(""))  
+
+    # image_title = ap.text.TextFragment("Desenho da solução")
+    # image_title.text_state.font_style = ap.text.FontStyles.BOLD
+    # page.paragraphs.add(image_title)
+    # image = ap.ImageStamp(image_path)
+    # image.height = 300 
+    # image.width = 300 
+    # image.x_indent = 150 
+    # image.y_indent = 490  
+   
+    # page.add_stamp(image)
+
+    image_position = ap.Rectangle(500, 100, 150, 1200, True)
+    page.add_image(image_path, image_position)
+   
+    pdf = "report/" + file_name + ".pdf"
+    
+    document.save(pdf)
 
 
 solve_vulnerabilities("imagem\Arquitetura2.jpg")
+
+# generate_report("imagem\Arquitetura2.jpg", "teste", "melhoria")
